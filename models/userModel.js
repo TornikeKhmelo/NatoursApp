21cad4eee3a6e68aa87683d6508a663b8d717e4b
Tornike Khmelidze <87017037+TornikeKhmelo@users.noreply.github.com>
2024-09-18 10:46:44 +0400

    start adding auth

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+    // unique: true,
+    // trim: true,
+    // maxlength: [40, "A tour name must have less or equal then 40 characters"],
+    // minlength: [10, "A tour name must have more or equal then 10 characters"],
+  },
+  slug: String,
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    mingLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm a password"],
+    mingLength: 8,
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+// document middleware runs only save and create
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+
+  // has the Password cost 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
